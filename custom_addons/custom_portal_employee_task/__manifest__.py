cffc8781580fb50dd1401e08ea9ba368119595d6
adnanalvi7 <adnanalvi68@gmail.com>
2025-04-03 16:31:31 +0500

    Warning fix

--- custom_addons/custom_portal_employee_task/__manifest__.py
+++ custom_addons/custom_portal_employee_task/__manifest__.py
@@ -4,12 +4,14 @@
     'category': 'Project',
     'summary': 'Allow portal users to be assigned as employees and receive login credentials',
     'author': 'Fazeel Malik',
-    'depends': ['base','hr_contract','project', 'hr', 'portal' ],
+    'depends': ['base','hr_contract','project', 'hr', 'portal' , 'hr_timesheet' ],
     'license': 'LGPL-3',
     'data': [
         'security/ir_model_access.xml',
         'views/employee_view.xml',
         'views/project_task_view.xml',
+        'security/hr_timesheet_security.xml',
+        'views/hr_employee_views.xml',
         
     ],
     'installable': True,
